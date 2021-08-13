cf0b66a49f04c0fb4734e19c07749eabfc4ce94b
David Mihal <david@dmihal.com>
2021-08-13 16:14:11 +0100

    Basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
 import styled from 'styled-components'
-import Layout from 'components/Layout'
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url('/logo.svg');
+  background-color: #012042;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding-top: 120px;
+  color: white;
+  font-family: sans-serif;
+  text-align: center;
+  font-size: 20px;
+  line-height: 28px;
+`
 
 const Title = styled.h1`
   color: red;
@@ -8,8 +29,9 @@ const Title = styled.h1`
 
 export default function Home() {
   return (
-    <Layout>
-      <Title>My page</Title>
-    </Layout>
+    <Container>
+      One neutral source of truth for crypto metrics.<br />
+      Used by everyone, managed by the community.
+    </Container>
   )
 }
