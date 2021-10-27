4f1f6aef278f4c3f9f793823fcef1cc1b31933f8
David Mihal <david@dmihal.com>
2021-10-27 17:27:10 +0100

    Allow dash in list ids

--- pages/api/list/[id].ts
+++ pages/api/list/[id].ts
@@ -3,7 +3,7 @@ import { getModulesForList } from 'utils/lists'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const listId = req.query.id.toString();
-  if (!/^[a-zA-Z]+$/.test(listId)) {
+  if (!/^[a-zA-Z-]+$/.test(listId)) {
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate');
     res.json({ success: false, error: `Invalid id ${listId}` })
     return
