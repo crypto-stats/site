ad8e12f664c26804b5cca9d82861301b18083017
David Mihal <david@dmihal.com>
2021-12-17 12:58:54 +0000

    Allow one IPFS upload to fail

--- utils/ipfs-upload.ts
+++ utils/ipfs-upload.ts
@@ -9,6 +9,15 @@ export async function saveToIPFS(file: string, name: string): Promise<string> {
     throw new Error('Pinata key missing')
   }
 
+  let failedUpload: string | null = null
+  const failHandler = (name: string) => () => {
+    if (failedUpload) {
+      throw new Error(`Uploads to ${failedUpload} and ${name} both failed`)
+    }
+    failedUpload = name
+    return { IpfsHash: null, path: null }
+  }
+
   const pinata = pinataSDK(process.env.PINATA_KEY, process.env.PINATA_SECRET)
   fs.writeFileSync(filePath, file);
   const pinataPromise = pinata.pinFromFS(filePath, {
@@ -19,22 +28,26 @@ export async function saveToIPFS(file: string, name: string): Promise<string> {
         type: 'module',
       },
     },
-  });
+  }).catch(failHandler('pinata'))
 
   const graphNode = create('https://api.thegraph.com/ipfs/api/v0' as any)
   const graphPromise = graphNode.add(file)
 
   const csNode = create('https://ipfs.cryptostats.community' as any)
-  const csPromise = csNode.add(file)
+  const csPromise = csNode.add(file).catch(failHandler('CryptoStats'))
 
   const [pinataResult, graphResult, csResult] = await Promise.all([pinataPromise, graphPromise, csPromise])
 
-  if (pinataResult.IpfsHash !== graphResult.path) {
+  if (failedUpload) {
+    console.warn(`2 out of 3 uploads successful, upload to ${failedUpload} failed`);
+  }
+
+  if (pinataResult.IpfsHash && graphResult.path && pinataResult.IpfsHash !== graphResult.path) {
     throw new Error(`Mismatched CIDs: ${pinataResult.IpfsHash} & ${graphResult.path}`)
   }
-  if (pinataResult.IpfsHash !== csResult.path) {
+  if (pinataResult.IpfsHash && csResult.path && pinataResult.IpfsHash !== csResult.path) {
     throw new Error(`Mismatched CIDs: ${pinataResult.IpfsHash} & ${csResult.path}`)
   }
 
-  return pinataResult.IpfsHash;
+  return pinataResult.IpfsHash || graphResult.path!;
 }
