155949a49c71e68b36692e5a29d2028f0078d009
Michele Prandina <michele.prandina@gmail.com>
2022-01-06 12:30:42 +0100

    change text style for "our investors" section

--- components/home-sections/Investors.tsx
+++ components/home-sections/Investors.tsx
@@ -9,7 +9,7 @@ const Investors: React.FC = () => {
       <ColumnSection from="2" to="12">
         <Text tag="h3" type="title_highlight" mb="40" align="center">Our investors</Text>
         <Text tag="p" type="title" align="center">None!</Text>
-        <Text tag="p" type="content" mt="40" align="center">CryptoStats is a community-driven project, with no backing by VCs or other investors.</Text>
+        <Text tag="p" type="description" mt="40" align="center">CryptoStats is a community-driven project, with no backing by VCs or other investors.</Text>
       </ColumnSection>
     </RowSection>
   )
