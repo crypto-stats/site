abc617ac8ef20b45ef6f24ff146f07b7379a0fb6
David Mihal <david@dmihal.com>
2022-01-06 14:12:19 +0000

    Show code samples on homepage

--- components/home-sections/Actions.tsx
+++ components/home-sections/Actions.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
-import HeroCard from './HeroCard'
 import CollectionCard from './CollectionCard'
 import RowSection from 'components/RowSection'
 import ColumnSection from 'components/ColumnSection'
 import Text from 'components/Text'
 import Button from 'components/Button'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript'
+// @ts-ignore
+import theme from 'react-syntax-highlighter/dist/cjs/styles/hljs/stackoverflow-dark'
 
+SyntaxHighlighter.registerLanguage('javascript', js)
+
+const sdkCode = `const { CryptoStatsSDK } = require('@cryptostats/sdk');
+
+(async function() {
+  const sdk = new CryptoStatsSDK({
+    moralisKey: '<your key>',
+  });
+  const list = sdk.getList('fees');
+  await list.fetchAdapters();
+
+  const result = await list.executeQuery('oneDayTotalFees', '2022-01-01');
+  console.log(result);
+})();`
+
+const restCode = `const url = 'https://api.cryptostats.community/api/v1/fees/oneDayTotalFees/2022-01-01';
+const response = await fetch(url);
+const json = await response.json();
+console.log(json);`
 
 const Graphic = styled.div<{ background?: string }>`
   aspect-ratio: 3 / 2;
@@ -19,7 +41,58 @@ const Graphic = styled.div<{ background?: string }>`
   ${({ background }) => background && `background-image: url('${background}');`}
 `
 
+const CodeCard = styled.div`
+  border-radius: 10px;
+  box-shadow: 0 10px 35px 16px rgba(0, 36, 75, 0.05), 0 2px 4px 0 rgba(0, 0, 0, 0.04);
+  border: solid 1px #ddd;
+  background-color: #fff;
+  overflow: hidden;
+`
+
+const CodeCardHeader = styled.div`
+  border: solid 1px #ddd;
+  background: white;
+  height: 53px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const Syntax = styled(SyntaxHighlighter)`
+  margin: 0;
+  padding: 16px 8px !important;
+`
+
+const Toggle = styled.button`
+  ${({ disabled }) => disabled ? `
+    background-color: #d6eaff;
+    color: #0477f4;
+    font-weight: 600;
+  ` : `
+    color: #4e4e4e;
+    background-color: #eef1f7;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #d6eaff;
+    }
+  `}
+  font-size: 12px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  padding: 6px 16px;
+  margin: 0 6px;
+`
+
+enum CodeType {
+  SDK,
+  REST,
+}
+
 const Actions: React.FC = () => {
+  const [codeType, setCodeType] = useState(CodeType.SDK)
+
   return (
     <> 
       <RowSection mt="120">
@@ -49,10 +122,17 @@ const Actions: React.FC = () => {
         <ColumnSection from="3" to="11">
           <Text tag="h3" type="title_highlight" mb="24" align="center">Consume and use data</Text>
           <Text tag="p" type="content_big" mb="32" align="center">Use trustworthy data metrics in your website or dapp. It's free and always will be</Text>
-          <HeroCard title="Uniswap Fees" subtitle="Preview">
-            <div>Adapter name</div>
-            <div>Uniswap</div>
-          </HeroCard>
+
+          <CodeCard>
+            <CodeCardHeader>
+              <Toggle disabled={codeType === CodeType.SDK} onClick={() => setCodeType(CodeType.SDK)}>CryptoStats SDK</Toggle>
+              <Toggle disabled={codeType === CodeType.REST} onClick={() => setCodeType(CodeType.REST)}>REST API</Toggle>
+            </CodeCardHeader>
+
+            <Syntax language="javascript" style={theme} showLineNumbers>
+              {codeType === CodeType.SDK ? sdkCode : restCode}
+            </Syntax>
+          </CodeCard>
           <Button variant="outline" size="large">Read the docs</Button>
         </ColumnSection>
       </RowSection>
