534747799592d3eef2fe93967cbfb2700fdd9dd9
Róbert Istók <robertistok94@gmail.com>
2022-03-25 16:43:49 +0100

    Update index.ts

--- resources/templates/index.ts
+++ resources/templates/index.ts
@@ -1,4 +1,6 @@
 // @ts-nocheck
 export { default as emptyAdapter } from '!raw-loader!./empty-adapter.txt'
+export { default as emptyMapping } from '!raw-loader!./empty-mapping.txt'
+export { default as emptySchema } from '!raw-loader!./empty-schema.txt'
 export { default as feeAdapter } from '!raw-loader!./fee-adapter.txt'
 export { default as apyAdapter } from '!raw-loader!./apy-adapter.txt'
