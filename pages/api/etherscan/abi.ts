389633dfda0330674a1f177991430a62ca3b9de5
Róbert Istók <robertistok94@gmail.com>
2022-05-20 20:45:40 -0400

    Remove unwanted code

--- pages/api/etherscan/abi.ts
+++ pages/api/etherscan/abi.ts
@@ -5,13 +5,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     throw new Error('Only POST requests allowed')
   }
 
-  console.log({ b: req.body })
-
-  // if (!req.headers.authorization) {
-  //   res.status(400).json({ error: 'Must provide Authorization' })
-  //   return
-  // }
-
   fetch(
     `https://api.etherscan.io/api?module=contract&action=getabi&address=${req.body.address}&apikey=${process.env.ETHERSCAN_KEY}`,
     {
@@ -22,8 +15,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       if (_res.headers.get('Content-Type')?.indexOf('application/json') !== 0) {
         res.status(500).end(await _res.text())
       }
-      // const res2 = await _res.json()
-      // console.log(res2)
       res.json(await _res.json())
     })
     .catch(err => res.status(500).json(err))
