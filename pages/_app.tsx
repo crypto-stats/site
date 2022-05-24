28f35f35c51a95da6687029897212ccb027399d1
Róbert Istók <robertistok94@gmail.com>
2022-05-24 13:20:11 -0600

    Add manrope

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -117,6 +117,10 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@100;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <GlobalStyle />
