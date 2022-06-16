7cc80aed830826298bf9e622201989773efaedbe
Róbert Istók <robertistok94@gmail.com>
2022-06-16 13:21:38 -0400

    Add basic loading and error handling

--- components/SubgraphEditor/SubgraphConfig/SelectedContract/SelectedContract.tsx
+++ components/SubgraphEditor/SubgraphConfig/SelectedContract/SelectedContract.tsx
@@ -126,25 +126,32 @@ export const SelectedContract = (props: SelectedContractProps) => {
   const contractHasEvents = eventsFromAbi.length > 0
 
   const [newEvent, setNewEvent] = useState({ show: false, signature: '' })
+  const [metadataLoading, setMetadataLoading] = useState(false)
 
   const fetchMetadata = async () => {
+    setMetadataLoading(true)
     const metadataReq = await fetch(
       `https://miniscan.xyz/api/contract?network=ethereum&address=${addresses[CHAIN_ID]}`
     )
     const metadata = await metadataReq.json()
 
-    if (!metadata.error) {
+    const contractSourceCodeNotVerified =
+      metadata?.data?.ABI === 'Contract source code not verified'
+
+    if (!metadata.error && !contractSourceCodeNotVerified) {
       updateContract({
         abi: JSON.parse(metadata.data.ABI),
         startBlocks: { [CHAIN_ID]: parseInt(metadata.data.StartBlock) },
         name: metadata.data.ContractName,
       })
-    } else if (metadata.success === false) {
+    } else if (metadata.success === false || contractSourceCodeNotVerified) {
       updateContract({
         // errorMessage: metadata.error,
         source: 'custom',
       })
     }
+
+    setMetadataLoading(false)
   }
 
   useEffect(() => {
@@ -197,6 +204,7 @@ export const SelectedContract = (props: SelectedContractProps) => {
           <Trash2 className="delete-link" size={16} onClick={() => deleteContract()} />
         </div>
         <span className="address">{addresses[CHAIN_ID]}</span>
+        {metadataLoading ? <span>Fetching contract metadata</span> : null}
         {startBlocks[CHAIN_ID] ? (
           <span className="address">Deployed on block {startBlocks[CHAIN_ID]}</span>
         ) : null}
@@ -207,7 +215,7 @@ export const SelectedContract = (props: SelectedContractProps) => {
               : `${
                   source === 'etherscan'
                     ? 'ABI automatically fetched from etherscan'
-                    : 'ABI uploaded manually'
+                    : 'ABI upload manually'
                 }`}
           </span>
           {showUploadButton ? (
@@ -229,8 +237,18 @@ export const SelectedContract = (props: SelectedContractProps) => {
         </StatusContainer>
       </Header>
       {compileError ? (
-        <ErrorState>Please fix all compiler errors before managing events</ErrorState>
-      ) : (
+        <ErrorState>
+          Please fix all compiler errors in the schema and mapping files before managing events
+        </ErrorState>
+      ) : null}
+
+      {!contractHasEvents ? (
+        <ErrorState>
+          {showUploadButton ? 'Please upload the ABI manually' : 'Contract has no events'}
+        </ErrorState>
+      ) : null}
+
+      {!compileError && contractHasEvents ? (
         <EventHandlerContainer>
           <div className="labels">
             <span>ABI</span>
@@ -283,7 +301,7 @@ export const SelectedContract = (props: SelectedContractProps) => {
             </ActionButton>
           </NewEventBtnContainer>
         </EventHandlerContainer>
-      )}
+      ) : null}
     </Root>
   )
 }
