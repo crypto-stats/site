238ebd6f649336eb3d7e14c6f945892596d8560a
David Mihal <david@dmihal.com>
2022-07-10 18:34:32 +0100

    Add Assemblyscript StdLib

--- hooks/useGeneratedFiles.ts
+++ hooks/useGeneratedFiles.ts
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import { generateContractFile, generateSchemaFile } from 'utils/graph-file-generator'
 import { SubgraphData } from './local-subgraphs'
+// @ts-ignore
+import assemblyscriptGlobals from '!raw-loader!assemblyscript/std/assembly/index.d.ts'
 
 export const useGeneratedFiles = (subgraph: SubgraphData | null) => {
   const [files, setFiles] = useState<{ content: string; filePath: string }[]>([])
 
   const generateFiles = async (subgraph: SubgraphData) => {
-    const _files: { content: string; filePath: string }[] = []
+    const _files: { content: string; filePath: string }[] = [
+      { filePath: 'file:///assembly.d.ts', content: assemblyscriptGlobals },
+    ]
 
     const schemaCode = await generateSchemaFile(subgraph.schema)
     _files.push({ content: schemaCode, filePath: 'file:///schema.ts' })
