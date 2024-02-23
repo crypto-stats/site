9c44d2a2d15960b8708dcc9aed13c3a327df60da
David Mihal <david@dmihal.com>
2024-02-23 20:48:43 +0000

    Remove CS IPFS server

--- utils/ipfs-upload.ts
+++ utils/ipfs-upload.ts
@@ -20,39 +20,19 @@ export function getInfuraNode() {
 }
 
 export async function saveToIPFS(file: string | Buffer): Promise<string> {
-  let failedUpload: string | null = null
-  const failHandler = (name: string) => () => {
-    if (failedUpload) {
-      throw new Error(`Uploads to ${failedUpload} and ${name} both failed`)
-    }
-    failedUpload = name
-    return { IpfsHash: null, path: null }
-  }
-
   const infuraNode = getInfuraNode()
   const infuraPromise = infuraNode.add(file)
 
   const graphNode = create('https://api.thegraph.com/ipfs/api/v0' as any)
   const graphPromise = graphNode.add(file)
 
-  const csNode = create('https://ipfs.cryptostats.community' as any)
-  const csPromise = csNode.add(file).catch(failHandler('CryptoStats'))
-
-  const [infuraResult, graphResult, csResult] = await Promise.all([
+  const [infuraResult, graphResult] = await Promise.all([
     infuraPromise,
     graphPromise,
-    csPromise,
   ])
-
-  if (failedUpload) {
-    console.warn(`2 out of 3 uploads successful, upload to ${failedUpload} failed`)
-  }
 
   if (infuraResult.path && graphResult.path && infuraResult.path !== graphResult.path) {
     throw new Error(`Mismatched CIDs: ${infuraResult.path} & ${graphResult.path}`)
-  }
-  if (infuraResult.path && csResult.path && infuraResult.path !== csResult.path) {
-    throw new Error(`Mismatched CIDs: ${infuraResult.path} & ${csResult.path}`)
   }
 
   return infuraResult.path || graphResult.path!
